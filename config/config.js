6db235edc71d4e402563b6dfb3fc5b5fa8e882cf
GreenDiscord <green@apolga.com>
2023-01-18 22:22:01 +0000

    s

--- config/config.js
+++ config/config.js
@@ -61,7 +61,7 @@ module.exports = {
   vlc: {
 
     // If no password is given, a random password is used
-    password: 'EEmm23?!',
+    password: 'passwordgoeshere',
 
     // This must correspond with the port VLC's web interface uses
     port: 8080,
